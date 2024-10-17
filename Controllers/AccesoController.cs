8ba42d660c8ad095ba6f8cd2ffaae2c158faac4b
Andres <Andres@DESKTOP-DPH7L7D>
2024-10-17 00:15:29 -0300

    log in + cambio en front

---
+++ Controllers/AccesoController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AppCuidandoPatitas.Controllers
+{
+    public class AccesoController : Controller
+    {
+        public IActionResult LogIn()
+        {
+            return View();
+        }
+    }
+}
